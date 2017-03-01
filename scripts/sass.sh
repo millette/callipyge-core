5dfcbf4125b42f36cd3d9d780aa2c4fc587f211b
Robin Millette <robin@millette.info>
2017-03-01 14:08:34 -0500

    Regenerate style.css with new Foundation.

--- scripts/sass.sh
+++ scripts/sass.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-node_modules/.bin/node-sass --include-path node_modules/foundation-sites/scss --output public/css scss/style.scss
+node_modules/node-sass/bin/node-sass --include-path node_modules/foundation-sites/scss --output public/css scss/style.scss
